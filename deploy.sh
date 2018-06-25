e7782f38e799b2f37e9d4ce3998680caa2e989ba
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2018-06-25 16:42:32 -0700

    checkpoint

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
+#!/usr/bin/env bash
 now
-URL=pbcopy
-now alias $URL www.uaprivacy.com
-now alias $URL uaprivacy.com
-now alias $URL www.uaprivacy.org
-now alias $URL uaprivacy.org
-
+URL=pbpaste
+# now alias $URL www.uaprivacy.com
+# now alias $URL uaprivacy.com
+# now alias $URL www.uaprivacy.org
+# now alias $URL uaprivacy.org
