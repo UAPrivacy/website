c6a69e52f83db4f665a4db4ba532080f6c5d4978
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2018-07-31 21:27:17 -0700

    auto deploy

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+now
+URL=$(pbpaste)
+now alias $URL www.uaprivacy.com
+now alias $URL uaprivacy.com
+now alias $URL www.uaprivacy.org
+now alias $URL uaprivacy.org
+now scale $URL 3 3
