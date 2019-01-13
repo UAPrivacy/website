4d6e6764cf11e03661c2b441678b7003a1677794
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2019-01-13 14:07:58 -0800

    speed refactor checkpoint

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 now
 URL=$(pbpaste)
 now alias $URL www.uaprivacy.com
-now alias www.uaprivacy.com uaprivacy.com
-now alias www.uaprivacy.com www.uaprivacy.org
-now alias www.uaprivacy.com uaprivacy.org
+now alias $URL uaprivacy.com
+now alias $URL www.uaprivacy.org
+now alias $URL uaprivacy.org
